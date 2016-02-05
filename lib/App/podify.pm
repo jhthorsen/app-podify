90037bb74084442dd1706247e078293ce656927f
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-02-05 11:28:01 +0100

    Add main module

---
+++ lib/App/podify.pm
@@ -0,0 +1,40 @@
+package App::podify;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+App::podify - Add POD to your modules
+
+=head1 VERSION
+
+0.01
+
+=head1 SYNOPSIS
+
+  $ podify lib/My/Module.pm
+  $ podify lib/My/Module.pm -i
+
+=head1 DESCRIPTION
+
+L<App::podify> is an application which can help you document your
+module.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014, Jan Henning Thorsen
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - C<jhthorsen@cpan.org>
+
+=cut
